51d9f0028be0dbf769d3ea2b79ca67367248839d
Ravula Akshith <ravulaakshith1@gmail.com>
2026-02-16 08:50:00 +0530

    feat: integrate backend analysis results in dashboard

--- lib/src/repo_analyzer.dart
+++ lib/src/repo_analyzer.dart
@@ -1,24 +1,34 @@
 // chore: initial project base - Change ID: 1116362076
-// chore: initial project base - Change ID: 2008138394
-// chore: initial project base - Change ID: 4168860763
 // chore: initial project base - Change ID: 188913200
 // chore: initial project base - Change ID: 2100212963
 // chore: initial project base - Change ID: 662414740
-// chore: initial project base - Change ID: 2766563595
 // chore: initial project base - Change ID: 2515568028
 // chore: initial project base - Change ID: 2110272069
 // chore: initial project base - Change ID: 939341892
 // chore: initial project base - Change ID: 654036206
-// chore: initial project base - Change ID: 3611066004
 // chore: initial project base - Change ID: 3116923404
 // chore: initial project base - Change ID: 2838303295
 // chore: initial project base - Change ID: 833479979
-// chore: initial project base - Change ID: 3896193891
-// chore: initial project base - Change ID: 2944550383
-// chore: initial project base - Change ID: 3202747181
 // chore: initial project base - Change ID: 3155851704
 // chore: initial project base - Change ID: 1074218475
 // chore: initial project base - Change ID: 2780013423
 // chore: initial project base - Change ID: 1795599523
 // chore: initial project base - Change ID: 1939892885
 // chore: initial project base - Change ID: 3134836126
+// feat: integrate backend analysis results in dashboard - Change ID: 2003496525
+// feat: integrate backend analysis results in dashboard - Change ID: 4021943739
+// feat: integrate backend analysis results in dashboard - Change ID: 4235344622
+// feat: integrate backend analysis results in dashboard - Change ID: 1361160565
+// feat: integrate backend analysis results in dashboard - Change ID: 1704736650
+// feat: integrate backend analysis results in dashboard - Change ID: 1471461562
+// feat: integrate backend analysis results in dashboard - Change ID: 1303447668
+// feat: integrate backend analysis results in dashboard - Change ID: 3749784060
+// feat: integrate backend analysis results in dashboard - Change ID: 3996129144
+// feat: integrate backend analysis results in dashboard - Change ID: 2394018985
+// feat: integrate backend analysis results in dashboard - Change ID: 1815884962
+// feat: integrate backend analysis results in dashboard - Change ID: 343258967
+// feat: integrate backend analysis results in dashboard - Change ID: 1620253376
+// feat: integrate backend analysis results in dashboard - Change ID: 1124731547
+// feat: integrate backend analysis results in dashboard - Change ID: 300559150
+// feat: integrate backend analysis results in dashboard - Change ID: 488882322
+// feat: integrate backend analysis results in dashboard - Change ID: 3152239628
