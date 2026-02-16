195a0c39ef4003dd2bde0c357c1aeee6d7f1b79e
Ravula Akshith <ravulaakshith1@gmail.com>
2026-02-16 11:53:00 +0530

    feat: create github repository model and services

--- lib/src/repo_analyzer.dart
+++ lib/src/repo_analyzer.dart
@@ -6,7 +6,6 @@
 // chore: initial project base - Change ID: 2110272069
 // chore: initial project base - Change ID: 939341892
 // chore: initial project base - Change ID: 654036206
-// chore: initial project base - Change ID: 3116923404
 // chore: initial project base - Change ID: 2838303295
 // chore: initial project base - Change ID: 833479979
 // chore: initial project base - Change ID: 3155851704
@@ -17,12 +16,10 @@
 // chore: initial project base - Change ID: 3134836126
 // feat: integrate backend analysis results in dashboard - Change ID: 2003496525
 // feat: integrate backend analysis results in dashboard - Change ID: 4021943739
-// feat: integrate backend analysis results in dashboard - Change ID: 4235344622
 // feat: integrate backend analysis results in dashboard - Change ID: 1361160565
 // feat: integrate backend analysis results in dashboard - Change ID: 1704736650
 // feat: integrate backend analysis results in dashboard - Change ID: 1471461562
 // feat: integrate backend analysis results in dashboard - Change ID: 1303447668
-// feat: integrate backend analysis results in dashboard - Change ID: 3749784060
 // feat: integrate backend analysis results in dashboard - Change ID: 3996129144
 // feat: integrate backend analysis results in dashboard - Change ID: 2394018985
 // feat: integrate backend analysis results in dashboard - Change ID: 1815884962
@@ -32,3 +29,17 @@
 // feat: integrate backend analysis results in dashboard - Change ID: 300559150
 // feat: integrate backend analysis results in dashboard - Change ID: 488882322
 // feat: integrate backend analysis results in dashboard - Change ID: 3152239628
+// feat: create github repository model and services - Change ID: 1450155071
+// feat: create github repository model and services - Change ID: 2983318357
+// feat: create github repository model and services - Change ID: 1695912040
+// feat: create github repository model and services - Change ID: 3921528101
+// feat: create github repository model and services - Change ID: 3790260514
+// feat: create github repository model and services - Change ID: 2347625246
+// feat: create github repository model and services - Change ID: 1005595385
+// feat: create github repository model and services - Change ID: 1861139950
+// feat: create github repository model and services - Change ID: 880567390
+// feat: create github repository model and services - Change ID: 3225355664
+// feat: create github repository model and services - Change ID: 3980067523
+// feat: create github repository model and services - Change ID: 1733710748
+// feat: create github repository model and services - Change ID: 4157381502
+// feat: create github repository model and services - Change ID: 4231951490
